b466411e9312f24ea08aae1e1b6963a902ee0b7c
disrootx <404x@disroot.org>
2026-08-17 13:18:34 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Railway test OK");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Listening on ${port}`);
+});
